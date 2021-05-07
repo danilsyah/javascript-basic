57361d30b803bc9bcabecc4e6b32c1426cae3919
danil <danilsyaharihardjo@gmail.com>
2021-05-07 13:42:28 +0700

    update

---
+++ coding-interview-fizz-buzz/index.js
@@ -0,0 +1,14 @@
+const play = (number) => {
+    for (let index = 1; index <= number; index++) {
+        if(index % 3  === 0 && index % 5 === 0){
+            console.log("Fizz Buzz");
+        }else if(index % 3 === 0){
+            console.log("Fizz")
+        }else if(index % 5 === 0){
+            console.log("Buzz")
+        }else{
+            console.log(index)
+        }
+    }
+}
+play(34)
--- play-with-npm/index.js
+++ play-with-npm/index.js
@@ -8,3 +8,28 @@ const total = _.size(myArray)
 console.log(sum)
 console.log(max)
 console.log(total)
+
+const capital = {
+    "Jakarta": "Indonesia",
+    "London": "England",
+    "Tokyo": "Japan"
+}
+capital["New Delhi"] = "Indonesia";
+
+console.log(capital["Indonesia"]);
+
+function fetchUsername() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('JSUser');
+        }, 3000);
+    })
+}
+
+console.log("Fetching username...");
+fetchUsername().then((value) => {
+    console.log(`You are logged in as ${value}`);
+})
+.finally(() => {
+    console.log("Welcome!");
+})
